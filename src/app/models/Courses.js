ead1e6cd94c10f178e7badd3919e1ac3eb15e531
Fayrun <luuphuc1368@gmail.com>
2026-02-23 10:47:11 +0700

    update

--- src/app/models/Courses.js
+++ src/app/models/Courses.js
@@ -7,5 +7,7 @@ const courseSchema = new Schema({
   image: { type: String, required: true, maxlength: 255 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  videoUrl: { type: String, required: true, maxlength: 255 },
+  slug: { type: String, required: true, maxlength: 255, unique: true },
 });
 module.exports = mongoose.model("Course", courseSchema);
