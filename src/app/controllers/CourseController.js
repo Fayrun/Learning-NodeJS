dcae2702d1e53bba35e5a14242355a614c0a7c99
Fayrun <luuphuc1368@gmail.com>
2026-02-23 10:13:28 +0700

    update

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,17 @@
+const Course = require("../models/Courses");
+class CourseController {
+  // Get single course
+  async show(req, res) {
+    try {
+      const course = await Course.findOne({ slug: req.params.slug });
+      if (!course) {
+        return res.status(404).json({ error: "Course not found" });
+      }
+      res.render("courses/show", { course: course.toObject() });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+module.exports = new CourseController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newsRouter = require("./NewsRoute");
+const coursesRouter = require("./CoursesRoute");
 const siteRouter = require("./SiteRoute");
 function route(app) {
   app.use("/news", newsRouter);
+  app.use("/courses", coursesRouter);
   app.use("/", siteRouter);
 }
 
